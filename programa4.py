01c32b17162db755269ae843051f01049aef4b3b
marcus6255@gmail.com <marcus6255@gmail.com>
2019-08-17 21:37:02 -0400

    quarto

---
+++ programa4.py
@@ -0,0 +1,8 @@
+n = int(input('Digite um número: '))
+if n<10:
+    print ('O  número {} que você digitou é menor do que 10'.format(n))
+else:
+    print ('O número {} que você digitou é maior do que 10'.format(n))
+
+
+
